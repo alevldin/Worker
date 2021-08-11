93eed2068d42345184768480ad0a788f78546ba7
Alexey Volodin <volodinay@rambler.ru>
2021-08-11 21:07:26 +0400

    init

---
+++ src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Worker.OnTaskDoneListener listener = System.out::println;
+        Worker.OnTaskErrorListener errorListener = System.out::println;
+        Worker worker = new Worker(listener, errorListener);
+        worker.start();
+    }
+}
